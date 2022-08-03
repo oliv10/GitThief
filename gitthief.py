f17b3136309d562391cc702315c781b27eddd42e
Oliver Scotten <revi99@me.com>
2022-08-03 16:38:17 +0000

    1.0.0

---
+++ gitthief.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+try:
+    import pyfiglet
+except ImportError:
+    pass
+import subprocess
+import argparse
+import os
+
+# Git Clone Tools
+def download(tool: str, path: str) -> subprocess.CompletedProcess:
+    BASE = ['git', 'clone']
+    name = tool.split("/")[-1]
+    path = [path+"/"+name]
+    if "http" not in tool:
+        url = ["https://www.github.com/"+tool]
+    else:
+        url = [tool]
+    cmd = BASE+url+path
+    return subprocess.run(cmd)
+
+# Install Tool Requierments (Only works for Python Tools)
+def install():
+    try:
+        os.listdir(args.PATH)
+    except FileNotFoundError:
+        print("Tools Directory Not Found")
+        exit()
+    for tool in os.listdir(args.PATH):
+        cmd = ['pip3', 'install', '-r', args.PATH+"/"+tool+"/requirements.txt"]
+        subprocess.run(cmd)
+
+def main(args):
+    print(TITLE)
+
+    # Install All Tools
+    if args.ALL:
+        for tool in TOOLS:
+            download(tool, args.PATH)
+        install()
+        exit()
+
+    # Dynamically Install Selected Tools
+    elif not args.ALL and not args.UPDATE:
+        for tool in TOOLS:
+            t = tool.split("/")[1].replace("-","_")
+            c = getattr(args, t)
+            if c:
+                download(tool, args.PATH)
+        install()
+        exit()
+
+    # Update Tools
+    elif not args.ALL and args.UPDATE:
+        try:
+            s = os.listdir(args.PATH)
+        except FileNotFoundError:
+            print("Tools Directory Not Found")
+            exit()
+        for tool in os.listdir(args.PATH):
+            os.chdir(args.PATH+"/"+tool)
+            cmd = ['git', 'pull']
+            subprocess.run(cmd)
+
+if __name__ == "__main__":
+    
+    __version__ = "1.0.0"
+    NAME = "GitTheif"
+    DESCRIPTION = "An installer and updater for Git repositories."
+    try:
+        TITLE = pyfiglet.figlet_format(NAME, font="stop") + f"\n{NAME} {__version__}\n\n{DESCRIPTION}\n"
+    except:
+        TITLE = f"{NAME} {__version__}\n\n{DESCRIPTION}\n"
+    TOOLS = []
+
+    parser = argparse.ArgumentParser(description=f"{TITLE}", formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('PATH', help='path to store tools')
+    parser.add_argument('-A', '--ALL', action='store_true', help='install all tools')
+    parser.add_argument('-U', '--UPDATE', action='store_true', help='update installed tools (this will update ALL tools)')
+
+    # Get Tools from Config
+    with open("config/TOOLS.conf", "r") as file:
+        config = file.readlines()
+    for tool in config:
+        TOOLS.append(tool.strip())
+
+    # Dynamically Create Install Arguments for each Tool
+    for tool in TOOLS:
+        t = tool.split('/')[-1]
+        a = '--' + t
+        h = 'install ' + t
+        parser.add_argument(a, action='store_true', help=h)
+    
+    args = parser.parse_args()
+
+    main(args)
